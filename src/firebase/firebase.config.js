146774c5d84afa1e19c80711176b7222c856d94a
Ridwan4D <ridwanul161@gmail.com>
2024-04-17 15:11:15 +0600

    env file created

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBbwmqg2Fm-zuoCT5w26k0-my_Dt5pT6rM",
-    authDomain: "the-reez-auth.firebaseapp.com",
-    projectId: "the-reez-auth",
-    storageBucket: "the-reez-auth.appspot.com",
-    messagingSenderId: "695909360187",
-    appId: "1:695909360187:web:7c38ed036a8ad5a5c9721e"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
